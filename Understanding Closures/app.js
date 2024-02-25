a9d11dc81f0a86e45cf57f1253ec7aed401e0093
saway11 <rgarcia20.m@gmail.com>
2024-02-24 19:57:15 -0800

    closures

---
+++ Understanding Closures/app.js
@@ -0,0 +1,20 @@
+function buildFunctions() {
+    var arr = [];
+    for (var i = 0; i < 3; i++) {
+
+        arr.push(
+            (function(j) {
+                return function() {
+                    console.log(j)
+                }
+            }(i))
+        )
+    }
+    return arr;
+}
+
+var fs = buildFunctions();
+
+fs[0]();
+fs[1]();
+fs[2]();
